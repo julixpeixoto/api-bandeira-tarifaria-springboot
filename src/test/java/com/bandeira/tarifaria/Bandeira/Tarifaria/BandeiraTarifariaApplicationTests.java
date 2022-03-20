cc2ab41098e2293589dcad2b05941e4199d12fe9
Julierme Peixoto <julierme2005@hotmail.com>
2022-03-20 10:41:45 -0300

    Firt commit

---
+++ src/test/java/com/bandeira/tarifaria/Bandeira/Tarifaria/BandeiraTarifariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandeira.tarifaria.Bandeira.Tarifaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandeiraTarifariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
